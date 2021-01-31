b80c132bb4f8056a34574ca25a80e4fdaef61264
gabrielkanaan <gabrielkanaan99@gmail.com>
2021-01-31 22:25:09 +0200

    Books Create/Index/New

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const Book = require("../models/book");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", async (req, res) => {
+  try {
+    books = await Book.find().sort({ createAt: "desc" }).limit(10).exec();
+  } catch {
+    books = [];
+  }
+  res.render("index", { books: books });
 });
 
 module.exports = router;
